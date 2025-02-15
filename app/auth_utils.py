5aac982fb9be468a1356a1d4a67621fcdd5e7ea7
Kirill <kirillbk@gmail.com>
2025-02-15 22:45:08 +0300

    add JWTBearer

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -1,6 +1,9 @@
 from app.config import settings
+from app.exceptions import UnauthorizedException
 
-from jwt import encode, decode
+from fastapi import Request
+from fastapi.security import HTTPBearer, utils
+from jwt import encode, decode, PyJWTError
 
 from datetime import datetime, timedelta, timezone
 
@@ -19,5 +22,22 @@ def encode_jwt_token(data: str) -> str:
     return token
 
 
-def decode_jwt_token(token: str) -> dict | None:
-    decode(token, settings.jwt_secret_key, (settings.jwt_algorithm, ))
+class JWTBearer(HTTPBearer):
+    def __init__(self):
+        super().__init__(bearerFormat="JWT")
+
+    async def __call__(self, request: Request) -> str:
+        authorization = request.headers.get("Authorization")
+        scheme, credentials = utils.get_authorization_scheme_param(authorization)
+        if not (authorization and scheme and credentials):
+            raise UnauthorizedException("Not authenticated")
+        if scheme.lower() != "bearer":
+            raise UnauthorizedException("Invalid authentication credentials")
+
+        try:
+            data = decode(
+                credentials, settings.jwt_secret_key, (settings.jwt_algorithm,)
+            )
+        except PyJWTError as e:
+            raise UnauthorizedException(str(e))
+        return data["sub"]
